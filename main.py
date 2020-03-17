4e45beb97a468d1e52e3634b49b121645b8c7152
LilMad0 <61454567+LilMad0@users.noreply.github.com>
2020-03-17 16:51:32 +0800

    update week 7

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from urllib.request import urlopen
+import json
+import datetime
 
-link = 'http://wttr.in/Perth?A'
-wget = urlopen(link)
-webtext = wget.read()
-print(webtext.decode('utf-8'))
+print("Welcome to cuz-weather")
+date = datetime.datetime.today()
+date = date.strftime("%Y/%-m/%-d")
+
+usersearch = input("Where would you like to check the weather?\n>")
+
+link = 'https://www.metaweather.com/api/location/search/?query=' + usersearch
+response = urlopen(link)
+data = json.loads(response.read())
+woeid = data[0]['woeid']
+
+link = "https://www.metaweather.com/api/location/" + str(woeid) + date
+response = urlopen(link)
+data = json.loads(response.read())
+
+print(data[0]['woeid'])
